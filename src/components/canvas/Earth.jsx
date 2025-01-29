cea8f5f51c3338462356b5d7a134a8d51ecbb013
sourav349 <kumarsourav2401@gmail.com>
2025-01-30 01:01:56 +0530

    Updating the Portfolio

---
+++ src/components/canvas/Earth.jsx
@@ -0,0 +1,55 @@
+import React, { Suspense, useRef } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+
+const Earth = () => {
+  const earthRef = useRef();
+  const earth = useGLTF("./planet/scene.gltf");
+
+  useFrame(() => {
+    // Rotate Earth along the Y-axis
+    if (earthRef.current) {
+      earthRef.current.rotation.y += 0.05; // Adjust this value for faster/slower rotation
+    }
+  });
+
+  return (
+    <primitive
+      ref={earthRef}
+      object={earth.scene}
+      scale={3}
+      position-y={0}
+      rotation-y={0}
+    />
+  );
+};
+
+const EarthCanvas = () => {
+  return (
+    <Canvas
+      shadows
+      frameloop="demand"
+      dpr={[1, 2]}
+      gl={{ preserveDrawingBuffer: true }}
+      camera={{
+        fov: 45,
+        near: 0.1,
+        far: 200,
+        position: [-4, 3, 6],
+      }}
+    >
+      <Suspense fallback={null}>
+        <OrbitControls
+          autoRotate
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Earth />
+        <Preload all />
+      </Suspense>
+    </Canvas>
+  );
+};
+
+export default EarthCanvas;
---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyD9GsY4rasXKGhmpSdSdYuRzfMb04Qkz6w",
+//   authDomain: "fir-image-d759c.firebaseapp.com",
+//   projectId: "fir-image-d759c",
+//   storageBucket: "fir-image-d759c.firebasestorage.app",
+//   messagingSenderId: "982111807688",
+//   appId: "1:982111807688:web:78cd9a59f680c33bda5dfd",
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
