b9b77eeaefdb5c1dfe946fd9300dcdb627c87e30
sourav349 <kumarsourav2401@gmail.com>
2025-01-30 12:27:31 +0530

    resume

--- src/data/constants.js
+++ src/data/constants.js
@@ -11,7 +11,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/sourav349",
   resume:
-    "https://drive.google.com/file/d/1txFKZD3LNIXMc329jD9c995AU8LItOXK/view?usp=drive_link",
+    "https://drive.google.com/file/d/1i-sEu-2b4uS1_9oCyJFjY_5ouf9N-IxB/view?usp=drivesdk",
   linkedin: "https://www.linkedin.com/in/sourav-kumar-133965294/",
   twitter: "https://x.com/SOURAVKUMA23059?s=09",
   insta: "https://www.instagram.com/sourav_agarwal_24/",
